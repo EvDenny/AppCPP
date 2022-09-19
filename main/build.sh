4d1fbc8fe0c26aacc7d91f374ad654fae8a67c38
Evan Denny <evandenny@outlook.com>
2022-09-18 21:28:17 -0400

    deleted extra files

--- main/build.sh
+++ main/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
+$CC=g++
 
-CC=g++
-LDFLAGS="$LDFLAGS"
-
-$CC App.cpp GuessV2.cpp hangman.cpp ArrayTutorial.cpp $LDFLAGS -o AppDebug0-1-4.1
+$CC App.cpp + clear.cpp + GuessV2.cpp + hangman.cpp + ArrayTutorial.cpp
