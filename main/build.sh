32dedd2c2887f336b36d06738fc17eaa8683e247
Evan Denny <107444879+EvDenny@users.noreply.github.com>
2022-10-04 00:40:44 -0400

    Update build.sh

--- main/build.sh
+++ main/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ ./main/App.cpp ./main/clear.cpp ./main/mdy.cpp ./main/hangman.cpp ./main/Guess.cpp -o AppDebugLinux
+g++ ./main/App.cpp ./main/clear.cpp ./main/mdy.cpp ./main/hangman.cpp ./main/Guess.cpp -o AppDebug
